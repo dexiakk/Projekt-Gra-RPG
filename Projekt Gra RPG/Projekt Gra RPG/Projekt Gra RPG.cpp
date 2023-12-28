5fd53b19e5e1aac546aebf28bd21b607463f654c
dexiakk <kacper.dekyyt@gmail.com>
2023-12-28 15:49:45 +0100

    Stworzenie repo dla Projektu

---
+++ Projekt Gra RPG/Projekt Gra RPG/Projekt Gra RPG.cpp
@@ -0,0 +1,20 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Dream RPG", sf::Style::Close | sf::Style::Titlebar);
+    while (window.isOpen())
+    {
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
+        {
+            if (evnt.type == evnt.Closed)
+            {
+                window.close();
+            }
+        }
+
+    }
+    
+    return 0;
+}
